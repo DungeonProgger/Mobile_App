d8cf5f9fee71a66509fe5f376c481f1147d8b964
DungeonProgger <bur.ilia2004@yandex.ru>
2025-04-17 14:54:31 +0300

    First Practic

---
+++ Lesson1/app/src/main/java/ru/mirea/BurmistrovIG/lesson1/MainActivity.java
@@ -0,0 +1,36 @@
+package ru.mirea.BurmistrovIG.lesson1;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        TextView myTextView = findViewById(R.id.textViewStudent);
+        Button button = findViewById(R.id.button);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        myTextView.setText("New text in MIREA");
+        button.setText("MireaButton");
+        checkBox.setChecked(true);
+    }
+}
